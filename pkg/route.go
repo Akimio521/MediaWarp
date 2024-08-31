6e128a58fbfd04eeafa783ecb64f3fa5b6b71d89
Akimio521 <akimio2333@gmail.com>
2024-08-31 23:40:27 +0800

    perfect(RegisterRoutesWithPrefixs):当未传入prefixs时，只注册原始路由

--- pkg/route.go
+++ pkg/route.go
@@ -6,11 +6,15 @@ import (
 
 // 多前缀注册路由
 func RegisterRoutesWithPrefixs(router *gin.Engine, rawPath string, handler gin.HandlerFunc, method string, prefixs ...string) {
-	for _, prefix := range prefixs {
-		path := prefix + rawPath
-
+	if len(prefixs) == 0 {
 		router.Handle(method, rawPath, handler)
-		router.Handle(method, path, handler)
+	} else {
+		for _, prefix := range prefixs {
+			path := prefix + rawPath
+
+			router.Handle(method, rawPath, handler)
+			router.Handle(method, path, handler)
+		}
 	}
 
 }
