c226b46e2196fa6e9bf2c3acfee4591678a9104f
Akimio521 <akimio2333@gmail.com>
2024-09-18 14:45:00 +0800

    style:增加一些常量的注释

--- constants/mediaserver.go
+++ constants/mediaserver.go
@@ -1,9 +1,9 @@
 package constants
 
-type MediaServerType string
+type MediaServerType string // 媒体服务器类型
 
 const (
-	EMBY     MediaServerType = "Emby"
-	JELLYFIN MediaServerType = "Jellyfin"
-	PLEX     MediaServerType = "Plex"
+	EMBY     MediaServerType = "Emby"     // 媒体服务器类型：EmbyServer
+	JELLYFIN MediaServerType = "Jellyfin" // 媒体服务器类型：Jellyfin
+	PLEX     MediaServerType = "Plex"     // 媒体服务器类型：Plex
 )
