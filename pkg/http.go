be4f40a787465d8f9df13da8c1578bec8a3e1c84
Akimio521 <akimio2333@gmail.com>
2024-09-21 15:33:53 +0800

    refactor(pkg):将http.go一些处理的字符串的函数移入string.go

--- pkg/http.go
+++ pkg/http.go
@@ -3,47 +3,9 @@ package pkg
 import (
 	"io"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
-
-// 分离主机域名（IP）和端口号
-//
-// 示例：
-// "example.com:8096" 					=> 	"example.com", "8096"
-// "[240e:da8:a801:5a47::316]:8096" 	=> 	"240e:da8:a801:5a47::316" "8096"
-// "192.168.1.1:8096" 					=> 	"192.168.1.1" "8096"
-func SplitHostPort(hostPort string) (host string, port string) {
-	host = hostPort
-
-	colon := strings.LastIndexByte(host, ':')
-	if colon != -1 && validOptionalPort(host[colon:]) {
-		host, port = host[:colon], host[colon+1:]
-	}
-
-	// 对地址是IPv6地址进行处理
-	if strings.HasPrefix(host, "[") && strings.HasSuffix(host, "]") {
-		host = host[1 : len(host)-1]
-	}
-
-	return
-}
-
-func validOptionalPort(port string) bool {
-	if port == "" {
-		return true
-	}
-	if port[0] != ':' {
-		return false
-	}
-	for _, b := range port[1:] {
-		if b < '0' || b > '9' {
-			return false
-		}
-	}
-	return true
-}
 
 // 代理访问上游服务器返回的响应体
 func GetRespBody(ctx *gin.Context, target string, api_key string) (body []byte, err error) {
