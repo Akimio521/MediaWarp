820c31c89045059a9a516d1a53fb0297c6187b07
Akimio521 <akimio2333@gmail.com>
2024-09-29 10:14:56 +0800

    style(constants):修改缓存类型常量

--- constants/cache.go
+++ constants/cache.go
@@ -3,6 +3,6 @@ package constants
 type CacheType string // 缓存类型
 
 var (
-	MEMORY_CACHE CacheType = "MemoryCache" // 内存缓存
-	REDIS_CACHE  CacheType = "RedisCache"  // Redis缓存
+	MEMORY_CACHE CacheType = "Memory" // 内存缓存
+	REDIS_CACHE  CacheType = "Redis"  // Redis缓存
 )
