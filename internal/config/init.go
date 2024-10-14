93f51d42a4e95cfac0c61bc8d32259e81ce83f55
Akimio521 <akimio2333@gmail.com>
2024-10-14 21:12:25 +0800

    update:移除NewConfig()，在GetConfig()中直接对logger进行初始化

--- internal/config/init.go
+++ internal/config/init.go
@@ -2,17 +2,11 @@ package config
 
 var cfg *ConfigManager // 全局配置实例
 
-// 实例化一个ConfigManager对象
-func NewConfig() *ConfigManager {
-	cm := ConfigManager{}
-	cm.Init()
-	return &cm
-}
-
 // 获取全局ConfigManager对象
 func GetConfig() *ConfigManager {
 	if cfg == nil {
-		cfg = NewConfig()
+		cfg = &ConfigManager{}
+		cfg.Init()
 	}
 	return cfg
 }
