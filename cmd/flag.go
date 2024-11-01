a01081a944891bdc864830160dd324cfd380213e
Akimio521 <akimio2333@gmail.com>
2024-11-01 15:37:04 +0800

    refactor: 移除旧的命令处理文件，整合初始化逻辑到 main.go

--- cmd/flag.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-import "flag"
-
-// 解析传入参数
-func InitFlag() {
-	flag.Parse()
-}
--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
-import "MediaWarp/cmd"
+import (
+	"MediaWarp/constants"
+	"MediaWarp/internal/config"
+	"MediaWarp/internal/logger"
+	"MediaWarp/internal/router"
+	"MediaWarp/pkg"
+	"flag"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	isDebug bool
+)
+
+func init() {
+	printLOGO()
+	flag.BoolVar(&isDebug, "debug", false, "是否启用调试模式")
+	flag.Parse()
+}
 
 func main() {
-	cmd.PrintLOGO()
-	cmd.InitFlag()
+	if isDebug {
+		logger.SetLevel(logrus.DebugLevel)
+		logger.Warning("已启用调试模式")
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
-	defer cmd.ShutDown()
-	cmd.SetUP()
+	logger.Info("MediaWarp 监听端口：", config.Port)
+	ginR := router.InitRouter() // 路由初始化
+	logger.Info("MediaWarp 启动成功")
+	ginR.Run(config.ListenAddr()) // 启动服务
+}
+
+// 打印LOGO
+func printLOGO() {
+	fmt.Print(
+		constants.LOGO,
+		pkg.Center(
+			fmt.Sprintf(" MediaWarp %s 启动中 ", config.Version()),
+			75,
+			"=",
+		),
+		"\n\n",
+	)
 }
