c72d52b574523c238845e463ac9682f2f6c8cef7
Akimio521 <akimio2333@gmail.com>
2024-12-22 21:33:02 +0800

    fix: 修改 VideosHandler 和 PlaybackInfoHandler 的匹配规则，避免匹配到字幕

--- constants/regexp.go
+++ constants/regexp.go
@@ -4,10 +4,10 @@ import "regexp"
 
 var EmbyRegexp = map[string]map[string]*regexp.Regexp{ // Emby 相关的正则表达式
 	"router": {
-		"VideosHandler":               regexp.MustCompile(`(?i)^(/emby)?/videos/(.*)/(stream|original)`),          // 普通视频处理接口匹配
+		"VideosHandler":               regexp.MustCompile(`(?i)^(/emby)?/videos/\d+/(stream|original)`),           // 普通视频处理接口匹配
 		"ModifyBaseHtmlPlayerHandler": regexp.MustCompile(`(?i)^/web/modules/htmlvideoplayer/basehtmlplayer.js$`), // 修改 Web 的 basehtmlplayer.js
 		"WebIndex":                    regexp.MustCompile(`^/web/index.html$`),                                    // Web 首页
-		"PlaybackInfoHandler":         regexp.MustCompile(`(?i)^(/emby)?/Items/(.*)/PlaybackInfo`),                // 播放信息处理接口
+		"PlaybackInfoHandler":         regexp.MustCompile(`(?i)^(/emby)?/Items/\d+/PlaybackInfo`),                 // 播放信息处理接口
 	},
 	"others": {
 		"VideoRedirectReg": regexp.MustCompile(`(?i)^(/emby)?/videos/(.*)/stream/(.*)`), // 视频重定向匹配，统一视频请求格式
