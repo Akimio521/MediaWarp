0624132d0c61b9d39d3a2ad80ed4697d96e50081
Akimio521 <akimio2333@gmail.com>
2025-03-22 23:27:56 +0800

    refactor(ClientFilter): 使用 switch 语句重构客户端过滤器逻辑，移除未知模式处理

--- internal/middleware/fliter.go
+++ internal/middleware/fliter.go
@@ -4,6 +4,7 @@ import (
 	"MediaWarp/constants"
 	"MediaWarp/internal/config"
 	"MediaWarp/internal/logging"
+	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -17,7 +18,8 @@ func ClientFilter() gin.HandlerFunc {
 		if userAgent == "" { // 开启了客户端过滤器后禁止所有未提供User-Agent的链接
 			allowed = false
 		} else {
-			if config.ClientFilter.Mode == constants.WHITELIST { // 白名单模式
+			switch config.ClientFilter.Mode {
+			case constants.WHITELIST: // 白名单模式
 				allowed = false
 				for _, ua := range config.ClientFilter.ClientList {
 					if strings.Contains(userAgent, ua) {
@@ -25,7 +27,7 @@ func ClientFilter() gin.HandlerFunc {
 						break
 					}
 				}
-			} else if config.ClientFilter.Mode == constants.BLACKLIST { // 黑名单模式
+			case constants.BLACKLIST: // 黑名单模式
 				allowed = true
 				for _, ua := range config.ClientFilter.ClientList {
 					if strings.Contains(userAgent, ua) {
@@ -33,15 +35,11 @@ func ClientFilter() gin.HandlerFunc {
 						break
 					}
 				}
-			} else {
-				logging.Error("未知的客户端过滤器模式，已关闭客户端过滤器")
-				config.ClientFilter.Enable = false
-				allowed = true
 			}
 		}
 
 		if !allowed {
-			ctx.AbortWithStatus(403) // 禁止访问
+			ctx.AbortWithStatus(http.StatusForbidden) // 禁止访问
 			logging.Info("客户端过滤器拦截了请求，User-Agent: ", userAgent)
 			return
 		}
