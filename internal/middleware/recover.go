ec05e64a51afa3231ab59d7106327efddc701706
Akimio521 <akimio2333@gmail.com>
2025-03-27 13:48:27 +0800

    feat: 在恢复中间件中添加详细的错误日志记录以处理 panic 错误

--- internal/middleware/recover.go
+++ internal/middleware/recover.go
@@ -1,27 +1,28 @@
 package middleware
 
 import (
+	"MediaWarp/internal/logging"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Recovery() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(ctx *gin.Context) {
 		defer func() {
-			if r := recover(); r != nil {
-				// 忽略 http.ErrAbortHandler 的 panic
-				if r == http.ErrAbortHandler {
-					return
-				}
-
-				// 处理其他 panic
-				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-					"error": "Internal Server Error",
-					"msg":   r,
-				})
+			if r := recover(); r != nil && r != http.ErrAbortHandler { // 忽略 http.ErrAbortHandler 的 panic（httputil.ReverseProxy 的 panic）
+				stack := debug.Stack()
+				logging.Errorf("[Recovery] %s panic revocered: %v\n%s", ctx.Request.URL.Path, r, string(stack))
+				ctx.AbortWithStatusJSON(
+					http.StatusInternalServerError,
+					gin.H{
+						"error": "MediaWarp Internal Server Error",
+						"msg":   r,
+					},
+				)
 			}
 		}()
-		c.Next()
+		ctx.Next()
 	}
 }
