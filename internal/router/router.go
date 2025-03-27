b9545763e74e525dfcd8eb3a70d8b5c81865b006
Akimio521 <akimio2333@gmail.com>
2025-03-27 09:47:35 +0800

    feat: 在正则路由处理器中添加调试日志以记录匹配成功的 URL

--- internal/router/router.go
+++ internal/router/router.go
@@ -55,6 +55,7 @@ func RegexpRouterHandler(ctx *gin.Context) {
 
 	for _, rule := range mediaServerHandler.GetRegexpRouteRules() {
 		if rule.Regexp.MatchString(ctx.Request.URL.Path) { // 不带查询参数的字符串：/emby/Items/54/Images/Primary
+			logging.Debug("URL: ", ctx.Request.URL.Path, "匹配成功 -> ", rule.Regexp.String())
 			rule.Handler(ctx)
 			return
 		}
