28a9329b31ce350ec1057fbb12354fadd529257b
Akimio521 <akimio2333@gmail.com>
2025-06-01 13:34:06 +0800

    fix(EmbyRegexp): 提高对 Afusekt 客户端的兼容性 #37

--- constants/regexp.go
+++ constants/regexp.go
@@ -21,7 +21,7 @@ type OthersRegexps struct {
 
 var EmbyRegexp = &EmbyRegexps{
 	Router: RouterRegexps{
-		VideosHandler:        regexp.MustCompile(`(?i)^(/emby)?/videos/\d+/(stream|original)$`),
+		VideosHandler:        regexp.MustCompile(`(?i)^(/emby)?/Videos/\d+/(stream|original)(\.\w+)?$`),
 		ModifyBaseHtmlPlayer: regexp.MustCompile(`(?i)^/web/modules/htmlvideoplayer/basehtmlplayer.js$`),
 		ModifyIndex:          regexp.MustCompile(`^/web/index.html$`),
 		ModifyPlaybackInfo:   regexp.MustCompile(`(?i)^(/emby)?/Items/\d+/PlaybackInfo$`),
