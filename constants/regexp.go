a7d0b5ceb1d02b8d9ee09754fb23873e36fe2cea
dinghuan <dinghuan@yy.com>
2025-07-23 17:05:05 +0800

    Jellyfin router

--- constants/regexp.go
+++ constants/regexp.go
@@ -44,9 +44,9 @@ type JellyfinRegexps struct {
 
 var JellyfinRegexp = &JellyfinRegexps{
 	Router: JellyfinRouterRegexps{
-		VideosHandler:      regexp.MustCompile(`/Videos/\w+/(stream|original)(\.\w+)?$`), // /Videos/813a630bcf9c3f693a2ec8c498f868d2/stream /Videos/205953b114bb8c9dc2c7ba7e44b8024c/stream.mp4
-		ModifyIndex:        regexp.MustCompile(`^/web/$`),
-		ModifyPlaybackInfo: regexp.MustCompile(`^/Items/\w+/PlaybackInfo$`),
-		ModifySubtitles:    regexp.MustCompile(`/Videos/\d+/\w+/subtitles$`),
+		VideosHandler:      regexp.MustCompile(`(/emby)?/Videos/\w+/(stream|original)(\.\w+)?$`), // /Videos/813a630bcf9c3f693a2ec8c498f868d2/stream /Videos/205953b114bb8c9dc2c7ba7e44b8024c/stream.mp4
+		ModifyIndex:        regexp.MustCompile(`^(/emby)?/web/$`),
+		ModifyPlaybackInfo: regexp.MustCompile(`^(/emby)?/Items/\w+/PlaybackInfo$`),
+		ModifySubtitles:    regexp.MustCompile(`(/emby)?/Videos/\d+/\w+/subtitles$`),
 	},
 }
