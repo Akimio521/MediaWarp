55f45bc35a6d1a34c6a506e152bd5f936cdff4fb
AkimioJR <akimio.jr@gmail.com>
2025-10-03 15:38:56 +0800

    refactor: 为 FliterMode 添加 JSON 和 YAML 的序列化与反序列化方法

--- constants/config.go
+++ constants/config.go
@@ -1,5 +1,13 @@
 package constants
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
 type FliterMode uint8
 
 const (
@@ -16,4 +24,62 @@ func (f FliterMode) String() string {
 	default:
 		return "Unknown"
 	}
+}
+
+func (f FliterMode) MarshalJSON() ([]byte, error) {
+	return json.Marshal(f.String())
+}
+
+func (f *FliterMode) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+	switch strings.ToLower(s) {
+	case "whitelist":
+		*f = WHITELIST
+	case "blacklist":
+		*f = BLACKLIST
+	default:
+		return fmt.Errorf("unknown FliterMode: %s", s)
+	}
+	return nil
+}
+
+func (f FliterMode) MarshalYAML() (any, error) {
+	return f.String(), nil
+}
+
+func (f *FliterMode) UnmarshalYAML(value *yaml.Node) error {
+	var s string
+	if err := value.Decode(&s); err != nil {
+		return err
+	}
+	switch strings.ToLower(s) {
+	case "whitelist":
+		*f = WHITELIST
+	case "blacklist":
+		*f = BLACKLIST
+	default:
+		return fmt.Errorf("unknown FliterMode: %s", s)
+	}
+	return nil
+}
+
+func (f FliterMode) MarshalText() ([]byte, error) {
+	return []byte(f.String()), nil
+}
+
+func (f *FliterMode) UnmarshalText(text []byte) error {
+	s := string(text)
+	switch strings.ToLower(s) {
+	case "whitelist":
+		*f = WHITELIST
+	case "blacklist":
+		*f = BLACKLIST
+	default:
+		return fmt.Errorf("unknown FliterMode: %s", s)
+	}
+	return nil
 }
