8b7056f4336e7fb07e40a55c6c07808c825593c0
AkimioJR <akimio.jr@gmail.com>
2025-10-04 18:45:22 +0800

    perfect: 优化 QueryCaseInsensitive 方法，简化查询参数处理逻辑

--- internal/middleware/case.go
+++ internal/middleware/case.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net/url"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -9,24 +10,10 @@ import (
 // 将请求查询参数的键转换为小写
 func QueryCaseInsensitive() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// 获取所有查询参数
-		queryParams := ctx.Request.URL.Query()
-
-		// 创建一个新的 map 来存储大小写不敏感的查询参数
-		caseInsensitiveParams := make(map[string][]string)
-		for key, values := range queryParams {
-			// 	将查询参数的键转换为小写，并存储值
-			caseInsensitiveParams[strings.ToLower(key)] = values
+		var queryParams url.Values
+		for key, values := range ctx.Request.URL.Query() {
+			queryParams.Add(strings.ToLower(key), strings.Join(values, ","))
 		}
-
-		// 清空原始查询参数并设置新的大小写不敏感的查询参数
-		ctx.Request.URL.RawQuery = ""
-		q := ctx.Request.URL.Query()
-		for key, values := range caseInsensitiveParams {
-			for _, value := range values {
-				q.Add(key, value)
-			}
-		}
-		ctx.Request.URL.RawQuery = q.Encode()
+		ctx.Request.URL.RawQuery = queryParams.Encode()
 	}
 }
