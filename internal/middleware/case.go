0adcdccf54e9f13e2ea9dc0a577b7e4afc255866
AkimioJR <akimio.jr@gmail.com>
2025-10-07 13:22:43 +0800

    fix: 未初始化 url.Values 空指针问题

--- internal/middleware/case.go
+++ internal/middleware/case.go
@@ -10,7 +10,7 @@ import (
 // 将请求查询参数的键转换为小写
 func QueryCaseInsensitive() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var queryParams url.Values
+		queryParams := make(url.Values)
 		for key, values := range ctx.Request.URL.Query() {
 			queryParams.Add(strings.ToLower(key), strings.Join(values, ","))
 		}
